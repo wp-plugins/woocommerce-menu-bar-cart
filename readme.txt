94b86a2edc2d2d6d25c33346caa4ba501f872926
jprummer <jprummer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-24 01:45:20 +0000

    Initial Plugin Upload
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-menu-bar-cart/trunk@643875 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== Woocommerce Nav Shopping Cart ===
+Contributors: jprummer 
+Donate link: http://www.wpovernight.com/donate
+Tags: woocommerce, menu, bar, cart, basket, header
+Requires at least: 3.4
+Tested up to: 3.5
+Stable tag: 1.0.1
+
+Automatically displays number of items and total cost of items in customer's basket.
+
+== Description ==
+
+This plugin installs a shopping cart button in the navigation bar. The plugin takes less than a minute to setup, 
+and only displays when the following two conditions are true:
+
+1. A menu name is set to **cart**. 
+1. At least one item is in the shopping cart.
+
+If one or both of these conditions are not met, the cart button will not display.
+
+Finally, the cart automatically conforms to your site's styles, leaving you with no extra work.
+
+**Note:** A version that includes a shopping cart image (via FontAwesome) is available free at http://www.wpovernight.com/plugins
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then 
+activate the Plugin from Plugins page. Once the plugin is activated navigate to Appearance > Menus. Change the name of your 
+header menu to **cart** and click save. Everything else is automatic!
+
+== Frequently Asked Questions ==
+
+= Why is nothing happening? =
+
+Remember to make sure the following conditions are true: 
+
+1. A menu name is set to **cart**. 
+1. At least one item is in the shopping cart.
---
+++ wc_cart_nav.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Menu Bar Shopping Cart
+Plugin URI: www.wpovernight.com/plugins
+Description: Woocommerce plugin that places a cart tab with number of items and total cost in the menu bar. Activate the plugin, and you're ready to go! Will automatically conform to your theme styles. Be sure that a menu is set to 'cart', otherwise the plugin will not work.
+Version: 1.0.1
+Author: Jeremiah Prummer
+Author URI: www.wpovernight.com/about
+License: GPL2
+*/
+/*  Copyright 2012 Jeremiah Prummer (email : jeremiah.prummer@yahoo.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+?>
+<?php
+
+add_filter('wp_nav_menu_cart_items','add_search_box_to_menu' , 10, 2);
+
+function add_search_box_to_menu( $items ) {
+global $woocommerce;
+$viewing_cart = 'View Your Shopping Cart';
+$cart_url = $woocommerce->cart->get_cart_url();
+$cart_contents = sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);
+$cart_total = $woocommerce->cart->get_cart_total();
+if ($cart_contents > 0) {
+$items .= '<li style="float: right"><a class="cart-contents" href="'. $cart_url .'" 
+title="'. $viewing_cart .'"><i class="icon-shopping-cart"></i> '. $cart_contents .'
+ - '. $cart_total .'</a></li>';
+
+    return $items;
+    }
+else {
+return $items;
+}
+}
+?>
