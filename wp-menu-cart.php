8af4aad9f1c849e8a844e38924a1f506969f03df
jprummer <jprummer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 06:38:56 +0000

    Version Update
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-menu-bar-cart/trunk@1052286 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-menu-cart.php
+++ wp-menu-cart.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: WP Menu Cart
+Plugin Name: WooCommerce Menu Cart
 Plugin URI: www.wpovernight.com/plugins
 Description: Extension for your e-commerce plugin (WooCommerce, WP-Ecommerce, Easy Digital Downloads, Eshop or Jigoshop) that places a cart icon with number of items and total cost in the menu bar. Activate the plugin, set your options and you're ready to go! Will automatically conform to your theme styles.
 Version: 2.5.4
